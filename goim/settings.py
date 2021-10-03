87cfa4fcb7023a046a4a04a152713cc3426bbda8
YONKEU KWEYA STEVE JUNIOR <yokwejuste@yahoo.com>
2021-10-03 14:16:48 +0100

    changes made

--- goim/settings.py
+++ goim/settings.py
@@ -21,12 +21,12 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'goim.herokuapp.com',
-    'wwww.goim.herokuapp.com',
+    # 'goim.herokuapp.com',
+    # 'wwww.goim.herokuapp.com',
     'www.goimrecords.company',
-    '0.0.0.0',
-    '127.0.0.1',
-    'https://goim.herokuapp.com/',
+    # '0.0.0.0',
+    # '127.0.0.1',
+    # 'https://goim.herokuapp.com/',
     'goimrecords.company',
     'https://goimrecords.company',
     'https://goimrecords.company',
