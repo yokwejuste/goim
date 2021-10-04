9beec4b56d686ce7dbdc0c766272be10fa4fff29
YONKEU KWEYA STEVE JUNIOR <yokwejuste@yahoo.com>
2021-10-04 02:54:41 +0100

    changes made

--- records/views.py
+++ records/views.py
@@ -84,6 +84,7 @@ def search_result(request):
         search = request.POST['search']
         results = \
             GoCustomerRegistration.objects.filter(
+                name__search=search,
                 name__contains=search
             )
         return render(request,
