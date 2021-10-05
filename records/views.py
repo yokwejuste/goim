c7beac00eddba42d07dc550d260e57a62eb6f291
YONKEU KWEYA STEVE JUNIOR <yokwejuste@yahoo.com>
2021-10-05 22:05:25 +0100

    changes made

--- records/views.py
+++ records/views.py
@@ -252,17 +252,8 @@ def send_files(request):
         'username': username,
     }
     if request.POST:
-        form = GoCustomerRegistrationForm(
-            request.POST,
-            request.FILES.getlist('documents'))
-        documents = request.FILES.getlist('documents')
+        form = GoCustomerRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
-            for document in documents:
-                new_doc = GoCustomerRegistrationDocuments(
-                    document=request.FILES['names'],
-                    name=request.POST['name']
-                )
-                new_doc.save()
             name = request.POST['name']
             email_d = request.POST['email']
             subject = f'Welcome to {name}.'
